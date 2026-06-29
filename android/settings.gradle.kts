bacf297a91f42cdcf2661f1947d9ba8625532d93
Shafi <jonyszone@gmail.com>
2026-06-29 13:17:50 +0600

    update Android Gradle config: compileSdk 36, Kotlin 2.1.0, add Flutter maven repo

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,12 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+    }
+}
+
 pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
@@ -19,7 +28,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 include(":app")
