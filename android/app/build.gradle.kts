43b700ab88a88a07257d8cfd0efb2828e1884856
Shafi <jonyszone@gmail.com>
2026-06-30 12:12:32 +0600

    fix: update AGP to 8.9.1, Gradle to 8.11.1, NDK to 28.2.13676358, enable core library desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,11 +6,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.face_detection_app"
+    namespace = "com.shafi.facerecognition"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -21,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.face_detection_app"
+        applicationId = "com.shafi.facerecognition"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -37,6 +38,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
